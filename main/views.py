af5ef508285724884fefbe342d9af92a5b76be99
addirossi <addirossi@gmail.com>
2020-11-18 12:08:07 +0600

    Add cbv

--- main/views.py
+++ main/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view(['GET'])
-def products_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True, context={'request': request})
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def products_list(request):
+#     products = Product.objects.all()
+#     serializer = ProductSerializer(products, many=True, context={'request': request})
+#     return Response(serializer.data)
+#
+#
+# class ProductsList(APIView):
+#     def get(self, request, format=None):
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True, context={'request': request})
+#         return Response(serializer.data)
+
+class ProductsList(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # obj -> {} -> '{}'
+    #
+    # '{}' -> {} -> obj
+
+    # 'GET', 'POST', 'PATCH', 'PUT', 'DELETE'
+
+    # /products/ "GET" - получение листинга
+    # /products/<id>/ "GET" - получение деталей продукта
+    # /products/ "POST" - создание продукта
+    # /products/<id>/ "PUT/PATCH" - изменение
+    # /products/<id>/ "DELETE" - удаление
+
+    # CRUD(Create, Retrieve, Update, Delete)
+
+# {'id': 1, "title": "Adidas Predator", "description": "Brand new cleats"}
+# /products/1/ - PUT "{'title': 'Adidas Predator Accelerator'}" -> {'title': 'Adidas Predator Accelerator'}
+# /product/1/ - PATCH "{'title': 'Adidas Predator Accelerator'}" -> {'id': 1, "title": "Adidas Predator Accelerator", "description": "Brand new cleats"}
 #
 # Product() -> {'id': 1, 'title': 'Adidas Originals', ....}
--- store/urls.py
+++ store/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from main.views import products_list
+from main.views import ProductsList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', products_list)
+    path('', ProductsList.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
